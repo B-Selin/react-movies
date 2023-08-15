29e6137ae176ecea9257300447d42c20e1d44f70
B-Selin <selin@bozkurt.ca>
2023-08-14 19:55:21 -0600

    app.jsx user state links

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,25 +2,30 @@ import { useState } from 'react'
 
 import './App.css'
 import LoginPage from '../LoginPage/LoginPage';
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   // Set up a user state variable in <App> that will keep track of the user. The user state should be initialed to null (no user) .
 
   const [user, setUser] = useState(null)
+
   return (
-    <main>
+    <div>
       {user ? (
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<MoviesListPage />} />
-          <Route path="/movies/:movieName" element={<MovieDetailPage />} />  
-          <Route path="/actors" element={<ActorsListPage />} />
-        </Routes>
+        <>
+          <NavBar />
+
+          <Routes>
+            <Route path="/" element={<MoviesListPage />} />
+            <Route path="/movies/:movieName" element={<MovieDetailPage />} />
+            <Route path="/actors" element={<ActorsListPage />} />
+          </Routes>
+        </>
       ) : (
         <Routes>
           <Route path="/" element={<LoginPage />} />
         </Routes>
       )}
-    </main>
+    </div>
   );
 }
