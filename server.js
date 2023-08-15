1d85406b1f72cfb0b6d37ce0d294521754f712b0
B-Selin <selin@bozkurt.ca>
2023-08-14 19:28:47 -0600

    fix express crash

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Put API routes here, before the "catch all" route
-app.use('/api/users', require('./routes/api/users'));
+
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
