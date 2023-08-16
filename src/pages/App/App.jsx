15ca71705c2ad95e38089303b3f91b4532dce9d9
B-Selin <selin@bozkurt.ca>
2023-08-15 20:58:15 -0600

    other things and stuff

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,8 +2,8 @@ import "./App.css";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
-import ActorPage from "../ActorPage/ActorPage";
-import MoviePage from "../MoviePage/MoviePage";
+import ActorListPage from "../ActorListPage/ActorListPage.jsx";
+import MoviesListPage from "../MoviesListPage/MoviesListPage";
 import MovieDetailPage from "../MovieDetailPage/MovieDetailPage";
 import LoginPage from "../LoginPage/LoginPage";
 import { movies } from "../../data.js";
@@ -20,12 +20,12 @@ export default function App() {
         <>
           <NavBar user={user} />
           <Routes>
-            <Route path="/" element={<MoviePage movies={movies} />} />
+            <Route path="/" element={<MoviesListPage movies={movies} />} />
             <Route
               path="/movies/:movieName"
               element={<MovieDetailPage movies={movies} />}
             />
-            <Route path="/actors" element={<ActorPage movies={movies} />} />
+            <Route path="/actors" element={<ActorListPage movies={movies} />} />
           </Routes>
         </>
       ) : (
